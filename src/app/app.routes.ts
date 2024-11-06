e429b028aeaaaca1d095954c9b06b99bce88c339
Quaniscoding <vq.2509.2003@gmail.com>
2024-11-06 22:48:33 +0700

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -37,7 +37,7 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    // canActivate: [authGuard],
+    canActivate: [authGuard],
     title: 'Dashboard',
     children: [
       {
